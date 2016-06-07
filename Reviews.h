6700cdce75677516932c71273eb31a0dad767171
Lum Naw <lumnaw07@gmail.com>
2016-06-06 21:59:04 -0700

    Update Reviews.h

--- Reviews.h
+++ Reviews.h
@@ -1,4 +1,29 @@
 #ifndef REVIEWS_H
 #define REVIEWS_H
 
+#include <string>
+#include "Users.h"
+
+class Reviews {
+private:
+    string review;
+    int rating;
+    User author;
+    string timePosted;
+    
+public:
+    Reviews();
+    Reviews(string r, int i, User a);
+    ~Reviews();
+    string getReview() {return review;}
+    int getRating() {return rating;}
+    User getAuthor() {return author;}
+    string getTimePosted() {return timePosted;}
+    
+    void setReviews(string review) {this -> review =review; }
+    void setRating(int rating) {this ->rating = rating; }
+    void setAuthor(User author) {this -> author = author;}
+    void setTimePosted(string timePosted) {this ->timePosted =timePosted; }
+  
+};
 #endif
